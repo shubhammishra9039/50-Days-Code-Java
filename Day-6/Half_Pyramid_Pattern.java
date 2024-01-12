ebe1086b270b12630aaee3b715a342208aa66e0a
Shubham Mishra <131137726+shubhammishra9039@users.noreply.github.com>
2024-01-12 21:26:16 +0530

    Day 6 Completed

---
+++ Day-6/Half_Pyramid_Pattern.java
@@ -0,0 +1,13 @@
+public class Half_Pyramid_Pattern {
+    public static void main(String[] args) {
+        for(int line=1;line<=4;line++)
+        {
+            for(int j=1; j<=line;j++)
+            {
+                System.out.print(j);
+
+            }
+            System.out.println();
+        }
+    }
+}
