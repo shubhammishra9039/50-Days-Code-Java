b27f43fa0377034d049154523785b45a0343a727
Shubham Mishra <shubhammishra6595@gmail.com>
2024-02-25 22:33:49 +0530

    Done

---
+++ Day-16/class_hierarchy/Motorcycle.java
@@ -0,0 +1,10 @@
+package class_hierarchy;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Motorcycle  extends Sound{
+    @Override
+    public void sounds() {
+        System.out.println("Sound Motorcycle");
+    }
+}
---
+++ Day-16/class_hierarchy/Use_Sound.java
@@ -0,0 +1,10 @@
+package class_hierarchy;
+
+public class Use_Sound {
+    public static void main(String[] args) {
+        Sound obj=new Car();
+              obj.sounds();
+              obj=new Motorcycle();
+              obj.sounds();
+    }
+}
