ab7228fe5d6f193ff88dbb592a074dd9659cd68d
Shubham Mishra <131137726+shubhammishra9039@users.noreply.github.com>
2024-08-10 22:27:54 +0530

    Add files via upload

---
+++ Day-37/Formate.java
@@ -0,0 +1,8 @@
+public class Formate {
+    public static void main(String[] args) {
+        
+String formattedString = String.format("My name is %s and I am %d years old.", "John", 25);
+System.out.println(formattedString);
+
+    }
+}
